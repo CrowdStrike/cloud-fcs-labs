20736cf61a6a38cb7c6f4c364be9c6fae2d3c5c5
Rob Solomon <49410392+solorob33@users.noreply.github.com>
2024-02-13 16:28:20 -0500

    added SSM params conditions

--- tools/startDeployInfra.sh
+++ tools/startDeployInfra.sh
@@ -1,17 +1,24 @@
 NC="\033[0;0m"
 
-#git clone https://github.com/CrowdStrike/devdays.git  #NOT APPLICABLE, here for reference.
+# git clone https://github.com/CrowdStrike/cloud-fcs-labs.git  #NOT APPLICABLE, here for reference.
+# cd cloud-fcs-labs/tools/
 
 env_up(){
 
+# Set AWS region. 
+#   Note: deployment to other regions has not been tested and may produce unexpected results.
+AWS_REGION='us-east-1'
+
+# set variables. These are written to SSM Parameter Store to enable repeat builds directly from CloudFormation using deployInfra.yaml.
 EnvHash=$(LC_ALL=C tr -dc a-z0-9 </dev/urandom | head -c 5)
 S3Bucket="fcslab-templates-${EnvHash}"
-#EnvHash=$(aws ssm get-parameter --name=EnvHash --query 'Parameter.Value' --output text)
-#S3Bucket=$(aws ssm get-parameter --name=S3Bucket --query 'Parameter.Value' --output text)
-AWS_REGION='us-east-1'
+StackName="fcslab-infrastack-${EnvHash}"
+
+aws ssm put-parameter --name=SSM-EnvHash --value="${EnvHash}" --type=String --overwrite --region=$AWS_REGION --allowed-pattern "^[a-zA-Z0-9]{5}$"
+aws ssm put-parameter --name=SSM-S3Bucket --value="${S3Bucket}" --region=$AWS_REGION --overwrite 
+aws ssm put-parameter --name=SSM-InfraStack --value="${StackName}" --region=$AWS_REGION --type=String --overwrite
+
 S3Prefix='deployInfra'
-#StackName=$(aws ssm get-parameter --name=InfraStack --query 'Parameter.Value' --output text)
-StackName="fcslab-stack-${EnvHash}"
 TemplateName='deployInfra.yaml'
 
    echo 
@@ -26,11 +33,12 @@ TemplateName='deployInfra.yaml'
    echo "Building AWS lab environment...$NC"
 
    aws cloudformation create-stack --stack-name $StackName --template-url https://${S3Bucket}.s3.amazonaws.com/${S3Prefix}/${TemplateName} --region $AWS_REGION --disable-rollback \
-   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
-   --parameters \
-   ParameterKey=S3Bucket,ParameterValue=S3Bucket \
-   ParameterKey=S3Prefix,ParameterValue=${S3Prefix} \
-   ParameterKey=EnvHash,ParameterValue=EnvHash
+   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND #\
+#    --parameters \
+#    ParameterKey=S3Bucket,ParameterValue=${S3Bucket} \
+#    ParameterKey=S3Prefix,ParameterValue=${S3Prefix} \
+#    ParameterKey=EnvHash,ParameterValue=${EnvHash} \
+#    ParameterKey=SetParams,Parameteralue=${SetParams}
 
     echo "The Cloudformation stack will take 20-30 minutes to complete.$NC"
     echo "\n\nCheck the status at any time with the command \n\naws cloudformation describe-stacks --stack-name $StackName --region $AWS_REGION$NC\n\n"
