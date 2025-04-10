01fcdc7d7d8d48e6320f3d7e5e8cd452c314242d
Gax <gax.theodorio@crowdstrike.com>
2025-04-10 13:04:18 -0400

    changed helm install script to install latest version

--- deployInfra/bastionSetup.sh
+++ deployInfra/bastionSetup.sh
@@ -1,71 +1,69 @@
 function setup_environment_variables() {
-    region=$(curl -sq http://169.254.169.254/latest/meta-data/placement/availability-zone/)
-    region=${region: :-1}
-    accountId=$(aws sts get-caller-identity | jq -r .Account)
-    # CS_CID_LOWER=$(echo $CS_CID | cut -d '-' -f 1 | tr '[:upper:]' '[:lower:]')
-    cd /etc/sudoers.d 
-    echo "ssm-user ALL=(ALL) NOPASSWD:ALL" >> ssm-agent-users
+  region=$(curl -sq http://169.254.169.254/latest/meta-data/placement/availability-zone/)
+  region=${region::-1}
+  accountId=$(aws sts get-caller-identity | jq -r .Account)
+  # CS_CID_LOWER=$(echo $CS_CID | cut -d '-' -f 1 | tr '[:upper:]' '[:lower:]')
+  cd /etc/sudoers.d
+  echo "ssm-user ALL=(ALL) NOPASSWD:ALL" >>ssm-agent-users
 }
 
 function install_kubernetes_client_tools() {
-    printf "\nInstall K8s Client Tools"
-    mkdir -p /usr/local/bin/
-    curl --retry 5 -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2025-01-10/bin/linux/amd64/kubectl
-    chmod +x ./kubectl
-    mv ./kubectl /usr/local/bin/
-    mkdir -p /root/bin
-    ln -s /usr/local/bin/kubectl /root/bin/
-    ln -s /usr/local/bin/kubectl /opt/aws/bin
+  printf "\nInstall K8s Client Tools"
+  mkdir -p /usr/local/bin/
+  curl --retry 5 -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2025-01-10/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  mv ./kubectl /usr/local/bin/
+  mkdir -p /root/bin
+  ln -s /usr/local/bin/kubectl /root/bin/
+  ln -s /usr/local/bin/kubectl /opt/aws/bin
 
-    # cat > /etc/profile.d/kubectl.sh <<EOF
-    # !/bin/bash
-    # source <(/usr/local/bin/kubectl completion bash)
-    # EOF
-    # chmod +x /etc/profile.d/kubectl.sh
-    
-    curl --retry 5 -o helm.tar.gz https://get.helm.sh/helm-v3.12.3-linux-amd64.tar.gz
-    tar -xvf helm.tar.gz
-    chmod +x ./linux-amd64/helm
-    mv ./linux-amd64/helm /usr/local/bin/helm
-    ln -s /usr/local/bin/helm /opt/aws/bin
-    rm -rf ./linux-amd64/
+  # cat > /etc/profile.d/kubectl.sh <<EOF
+  # !/bin/bash
+  # source <(/usr/local/bin/kubectl completion bash)
+  # EOF
+  # chmod +x /etc/profile.d/kubectl.sh
 
-    # Install awscli v2
-    curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
-    unzip -o awscli-exe-linux-x86_64.zip
-    sudo ./aws/install
-    rm awscli-exe-linux-x86_64.zip
-    mv /bin/aws /bin/aws.v1
-    ln -s /usr/local/aws-cli/v2/current/dist/aws /bin/aws
+  curl --retry 5 -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+  rm ./get_helm.sh
+
+  # Install awscli v2
+  curl -O "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+  unzip -o awscli-exe-linux-x86_64.zip
+  sudo ./aws/install
+  rm awscli-exe-linux-x86_64.zip
+  mv /bin/aws /bin/aws.v1
+  ln -s /usr/local/aws-cli/v2/current/dist/aws /bin/aws
 }
 
 function setup_kubeconfig() {
 
-    printf "\nKube Config:\n"
-    
-    aws eks update-kubeconfig --name fcs-lab --region ${region}
+  printf "\nKube Config:\n"
 
-    # Add SSM Config for ssm-user
-    /sbin/useradd -d /home/ssm-user -u 1001 -s /bin/bash -m --user-group ssm-user
-    mkdir -p /home/ssm-user/.kube/
-    cp ~/.kube/config /home/ssm-user/.kube/config
-    cp /etc/profile.d/kubectl.sh /home/ssm-user/  
-    
-    instanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-    stackName=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=aws:cloudformation:stack-name" --query 'Tags[*].Value' --output text)
-    EnvHash=${stackName:16:5}
-    S3Bucket=$(aws ssm get-parameter --name "psS3Bucket" --query 'Parameter.Value' --output text --region=$region)
-    cd /tmp
-    aws s3 cp s3://$S3Bucket/tools/StackDeletionCleanup.sh StackDeletionCleanup.sh
-    cp ./StackDeletionCleanup.sh /home/ssm-user/ 
-    chown -R ssm-user:ssm-user /home/ssm-user/
-    chmod -R og-rwx /home/ssm-user/.kube
-    chmod +x /home/ssm-user/StackCleanup.sh
+  aws eks update-kubeconfig --name fcs-lab --region ${region}
+
+  # Add SSM Config for ssm-user
+  /sbin/useradd -d /home/ssm-user -u 1001 -s /bin/bash -m --user-group ssm-user
+  mkdir -p /home/ssm-user/.kube/
+  cp ~/.kube/config /home/ssm-user/.kube/config
+  cp /etc/profile.d/kubectl.sh /home/ssm-user/
+
+  instanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+  stackName=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$instanceId" "Name=key,Values=aws:cloudformation:stack-name" --query 'Tags[*].Value' --output text)
+  EnvHash=${stackName:16:5}
+  S3Bucket=$(aws ssm get-parameter --name "psS3Bucket" --query 'Parameter.Value' --output text --region=$region)
+  cd /tmp
+  aws s3 cp s3://$S3Bucket/tools/StackDeletionCleanup.sh StackDeletionCleanup.sh
+  cp ./StackDeletionCleanup.sh /home/ssm-user/
+  chown -R ssm-user:ssm-user /home/ssm-user/
+  chmod -R og-rwx /home/ssm-user/.kube
+  chmod +x /home/ssm-user/StackCleanup.sh
 
 }
 
-function setup_nodesensor_config(){
-    cat >/tmp/node_sensor.yaml <<EOF
+function setup_nodesensor_config() {
+  cat >/tmp/node_sensor.yaml <<EOF
 apiVersion: falcon.crowdstrike.com/v1alpha1
 kind: FalconNodeSensor
 metadata:
@@ -80,12 +78,12 @@ spec:
     tags: 
     - fcs-lab
 EOF
-    # To install Node Sensor, run the following command:
-    # kubectl create -f /tmp/node_sensor.yaml
+  # To install Node Sensor, run the following command:
+  # kubectl create -f /tmp/node_sensor.yaml
 
-    cp /tmp/node_sensor.yaml /home/ssm-user/ 
-    chown -R ssm-user:ssm-user /home/ssm-user/
-    chmod og-rwx /home/ssm-user/node_sensor.yaml
+  cp /tmp/node_sensor.yaml /home/ssm-user/
+  chown -R ssm-user:ssm-user /home/ssm-user/
+  chmod og-rwx /home/ssm-user/node_sensor.yaml
 }
 
 setup_environment_variables
